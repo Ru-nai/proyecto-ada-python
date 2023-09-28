922e765c007e9808ca8d68edf4e7210855db0591
Laura <lllauracb@gmail.com>
2023-09-27 19:42:01 -0500

    ,

--- main.py
+++ main.py
@@ -1,18 +1,1 @@
-'''
----------------------------------------------------------------------------------
-* Instalar la librería: https://pypi.org/project/readchar/
-* Investigrar cómo leer un caracter del teclado
-* Escribir un programa que corra un bucle infinito leyendo e imprimiento las teclas,
- y sólo terminará cuando se presione la tecla ↑ indicada como UP
----------------------------------------------------------------------------------
-'''
-'''Leer un caracter suelto del teclado, imprimirlo siempre que no sea la tecla arriba y detenerse al presionar dicha tecla arriba'''
-import readchar
-from readchar import readkey, key
 
-
-tecla = readchar.readkey()
-if tecla == readchar.key.UP:
-    print("Se presiono ↑")
-else:
-    print("Se presiono otra tecla")
