63fe13eb9cf8cd8a54c15ee8151b0bef9691c4c1
Laura <lllauracb@gmail.com>
2023-09-27 19:38:02 -0500

    -

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 jugador_nick = input('Ingresa tu nombre aquí: ') #Solicita el nombre del jugador
 
-print(f'¡Bienvenido, {jugador_nick}!') #Imprime mensaje de bienvenida + el nickname
+print(f'¡Bienvenido, {jugador_nick}!') #Imprime mensaje de bienvenida + el nickname.
