1415c2a3b8b426301cac86a2f87fe378e7e091ee
Laura <lllauracb@gmail.com>
2023-10-13 16:12:25 -0500

    main_loop

--- main.py
+++ main.py
@@ -40,6 +40,8 @@ laberinto_3 = "..###########\n........#...#\n#######.#.###\n#...........\n######
 opciones_laberinto = [laberinto_1, laberinto_2, laberinto_3]#Almacena los laberintos en una lista
 selecciona_laberinto_azar = random.choice(opciones_laberinto) #Se escoge un laberinto de 'opciones_laberinto' al azar con random, y se almacena dicho laberinto en 'selecciona_laberinto_azar'
 
+#------------------------------------------------------------------------------------------------------------
+#FUNCIONES:
 def obtener_tamano_laberinto(laberinto):
     #Función para obtener el tamaño del laberinto. Recibe como parámetro el laberinto y es para ayudar a hallar cuál sería la coordenada (end, end) si los laberintos son de distintos tamaños
     filas = laberinto.strip().split('\n') #Elimina espacios al inicio y al final del laberinto y separa el str en el salto de línea para crear distintas listas que funcionarán como filas, y  almacena dichas filas en la variable 'filas'
@@ -47,12 +49,44 @@ def obtener_tamano_laberinto(laberinto):
     num_columnas = max(len(fila) for fila in filas) #calcula el número de columnas dentro de las filas del laberinto. Tiene un bucle 'for' que recorre cada fila de la lista 'filas'; 'len(fila)' calcula la longitud de cada fila en la que está iterando, es decir, la cantidad de carácteres dentro de cada fila. 'max()' encuentra el valor máximo de la longitud de las filas, y retorna el número máximo de columnas.
     return num_filas, num_columnas #retorna una tupla que contiene (num_filas, num_columnas)
 
+
 def limpiar_consola():
     #Función para limpiar la pantalla/consola antes de imprimir / mostrar el laberinto usando la librería os.
     os.system('cls' if os.name == 'nt' else 'clear')
+
 
 def mostrar_laberinto(mapa):
     #Función para mostrar el laberinto en la consola luego de limpiarla. Recibe como parámetro 'mapa' que es una matriz que representa el laberinto
     limpiar_consola() #llama a la función 'limpiar_consola' antes de imprimir
     for fila in mapa: #bucle for que recorre cada fila (lista de listas) de la matriz 'mapa'
         print(''.join(fila)) #cada fila de la matriz 'mapa' se convierte a una cadena, para que se impriman como str en lugar de listas y se vea más claro, y las va imprimiendo
+
+
+def main_loop(mapa: List[List[str]], posicion_inicial: Tuple[int, int], posicion_final: Tuple[int, int]):
+    #define la funcion del main_loop, toma tres argumentos: 'mapa', el cual es una matriz que representa el laberinto; 'posicion_inicial' la cual es una tupla que contiene las posiciones inicial del jugador en el eje x y el eje y. Por último, 'posicion_final' contiene la posicion final de 'P'
+    px, py = posicion_inicial #Se asigna a las variable 'px' y 'py' las coordenadas de 'posicion_inicial'. Se usarán para rastrear la posicion actual de 'P'
+
+    while (px, py) != posicion_final: #inicia un bucle 'while' que se ejecute siempre y cuando px y py sean diferentes a la posicion_final
+        mapa[px][py] = 'P' #asigna el caracter 'P' a las 'coordenadas' actuales de px y py dentro del mapa
+        mostrar_laberinto(mapa) #llama a la función 'mostrar_laberinto'. Lo que muestra es el laberinto con la posicion actual de 'P'
+
+        tecla_presionada = readchar.readkey() #asigna la tecla presionada por el usuario a la variable 'tecla_presionada' usando readchar.readkey
+        current_px, current_py = px, py #la posicion actual de 'P' en X y Y se basa en la posicion px, py señalada anteriormente, para determinar donde se encuentra 'P'
+
+        tecla_presionada = readchar.readkey()#asigna la tecla presionada por el usuario a la variable 'tecla_presionada' usando readchar.readkey
+        if tecla_presionada == readchar.key.UP:
+            current_px -= 1 #Si la flecha presionada fue la tecla arriba, reduce current_px en 1
+        elif tecla_presionada == readchar.key.DOWN:
+            current_px += 1 #Si la flecha presionada fue la tecla abajo, incrementa current_px en 1
+        elif tecla_presionada == readchar.key.LEFT:
+            current_py -= 1 #Si la flecha presionada fue la tecla izquierda, reduce current_py en 1
+        elif tecla_presionada == readchar.key.RIGHT:
+            current_py += 1 #Si la flecha presionada fue la tecla derecha, incrementa current_py en 1
+
+        if 0 <= current_px < len(mapa) and 0 <= current_py < len(mapa[0]) and mapa[current_px][current_py] != '#':
+            #este if verifica si 'current_px' y 'current_py' está dentro del tamaño del laberinto, comprobando si current_px y current_py se encuentran entre 0 y el número de columnas que haya en el laberinto
+            #por alguna razon, este if no esta funcionando bien, deja que 'P' se salga de la matriz y da un error por consola
+            mapa[px][py] = '.'  # si después de que el 'if' evalúe, la posición del 'P' cumple las condiciones, reemplaza 'P' por '.'
+            px, py = current_px, current_py  # mueve a 'P' a la nueva posicion válida luego de reemplazar su posicion anterior por '.'
+
+
