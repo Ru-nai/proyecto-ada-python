233c9f24fe4cef9580175127484f5b3881388d4e
Laura C. Bernal <lllauracb@gmail.com>
2023-10-25 20:21:26 -0500

    .

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ import os
 import random
 from typing import List, Tuple
 
-jugador_nick = input('Ingresa tu nombre aquí: ') #Solicita el nombre del jugador
+'''jugador_nick = input('Ingresa tu nombre aquí: ') #Solicita el nombre del jugador
 
 print(f'¡Bienvenido, {jugador_nick}!') #Imprime mensaje de bienvenida + el nickname..'''
 
 
 '''Leer un caracter suelto del teclado, imprimirlo siempre que no sea la tecla arriba y detenerse al presionar dicha tecla arriba'''
 
-continuar = True #variable para cambiar a Falso y detener el loop
+'''continuar = True #variable para cambiar a Falso y detener el loop
 
 while continuar == True: #loop infinito
     tecla = readchar.readkey()
@@ -20,11 +20,11 @@ while continuar == True: #loop infinito
     if tecla == readchar.key.UP: #si se mantiene presionada la flecha arriba:
         continuar == False #la variable ligada al ciclo cambia a falso
         print ("Se presionó la tecla: ↑")
-        break #y se rompe el ciclo.
+        break #y se rompe el ciclo.'''
 
 '''Ejercicio: iniciar con un número 0, leer la tecla n por el teclado, por cada presionada borrar consola e imprimir nuevo numero hasta 50'''
 
-def clear_cons(tecla_n):
+'''def clear_cons(tecla_n):
     if tecla_n == 'n':
         os.system('cls' if os.name == 'nt' else 'clear')
         print (iter)
@@ -32,19 +32,19 @@ def clear_cons(tecla_n):
 
 for iter in range(0,51):
     clear_cons(tecla_n=readchar.readkey())
-
+'''
 ''' PROYECTO INTEGRADOR PARTE 4: GENERAR LABERINTO Y RECORRIDO'''
 #laberintos creados con dcode:
-laberinto_1 = "..#####\n......#\n###.#.#\n#...#.#\n###.###\n#...#.#\n#.#.#.#\n#.#...#\n###.###\n#.....\n######"
+'''laberinto_1 = "..#####\n......#\n###.#.#\n#...#.#\n###.###\n#...#.#\n#.#.#.#\n#.#...#\n###.###\n#.....\n######"
 laberinto_2 = "..###############\n..#.#.......#...#\n#.#.###.#.#.###.#\n#.......#.#.#.#.#\n#.#####.#.###.#.#\n#.....#.#.......\n################"
 laberinto_3 = "..###########\n........#...#\n#######.#.###\n#...........\n############"
 
 opciones_laberinto = [laberinto_1, laberinto_2, laberinto_3]#Almacena los laberintos en una lista
 selecciona_laberinto_azar = random.choice(opciones_laberinto) #Se escoge un laberinto de 'opciones_laberinto' al azar con random, y se almacena dicho laberinto en 'selecciona_laberinto_azar'
-
+'''
 #------------------------------------------------------------------------------------------------------------
 #FUNCIONES:
-def obtener_tamano_laberinto(laberinto):
+'''def obtener_tamano_laberinto(laberinto):
     filas = laberinto.strip().split('\n')
     num_filas = len(filas)
     num_columnas = max(len(fila) for fila in filas)
@@ -53,15 +53,15 @@ def obtener_tamano_laberinto(laberinto):
 
 def limpiar_consola():
     os.system('cls' if os.name == 'nt' else 'clear')
+'''
 
-
-def mostrar_laberinto(mapa):
+'''def mostrar_laberinto(mapa):
     limpiar_consola()
     for fila in mapa:
         print(''.join(fila))
+'''
 
-
-def main_loop(mapa: List[List[str]], posicion_inicial: Tuple[int, int], posicion_final: Tuple[int, int]):
+'''def main_loop(mapa: List[List[str]], posicion_inicial: Tuple[int, int], posicion_final: Tuple[int, int]):
     px, py = posicion_inicial
     while (px, py) != posicion_final:
         mapa[px][py] = 'P'
@@ -84,8 +84,8 @@ def main_loop(mapa: List[List[str]], posicion_inicial: Tuple[int, int], posicion
             #por alguna razon, este if no esta funcionando bien, deja que 'P' se salga de la matriz y da un error por consola
             mapa[px][py] = '.'
             px, py = current_px, current_py
-
-
+'''
+'''
 def ejecutar():
     laberinto_seleccionado = selecciona_laberinto_azar #se le asigna el valor de la variable 'selecciona_laberinto_azar' a la variable 'laberinto_seleccionado'. De esta manera, se le pasará uno de los tres laberintos definidos al inicio del código
     tamano = obtener_tamano_laberinto(laberinto_seleccionado) #'tamano' almacena la tupla obtenida en la funcion 'obtener_tamano_laberinto' para luego 'tamano' para establecer la coordenada (end,end)
@@ -95,4 +95,4 @@ def ejecutar():
 
     main_loop(mapa, posicion_inicial, posicion_final) #finalmente, se llama a la funcion "main_loop' y se le pasan como parámetros las variables que se definieron antes de llamar a dicha funcion dentro de 'ejecutar'
 
-ejecutar() #llama a la funcion 'ejecutrar' para que ejecute todo lo que tiene adentro
+ejecutar() #llama a la funcion 'ejecutrar' para que ejecute todo lo que tiene adentro'''
